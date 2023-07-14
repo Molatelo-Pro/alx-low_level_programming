c9506139ffda543bdd1870a996863c37ef60580c
Molatelo-Pro <molatelomashishi@gmail.com>
2023-07-14 03:30:27 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Programming that prints a putchar function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
