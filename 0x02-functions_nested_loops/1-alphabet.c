4aa279942c281a44a199016b851be03e96c81c74
Molatelo-Pro <molatelomashishi@gmail.com>
2023-07-18 17:41:36 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,13 @@
  * main - function that prints the alphabet, in lowercase
  * return: void
  */
+void print_alphabet(void)
 {
 char c;
 
 for (c = 'a'; c <= 'z'; c++)
 {
-	_putchar(c);
+_putchar(c);
 }
 _putchar('\n');
 
