33e022c51219bec6b863f7704d1272bb75b83547
Molatelo-Pro <molatelomashishi@gmail.com>
2023-08-16 11:44:06 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked -  function that allocates memory using malloc
+ * @b: unsigned integer.
+ * Return: pointer to the allocated memory.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+int *ptr;
+
+ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+return (ptr);
+}
