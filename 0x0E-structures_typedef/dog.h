50f3c9b3a07ed25f706d162190f8bef0110b2569
Molatelo-Pro <molatelomashishi@gmail.com>
2023-08-29 08:19:48 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct_dog - Define a new type,
+ * with the following elements:
+ * @name: Dog name.
+ * @age: Dog name.
+ * @owner: Dog owner.
+ * Return: elements of the dog.
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+/**
+ * dog_t - typedef struct dog.
+ */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
