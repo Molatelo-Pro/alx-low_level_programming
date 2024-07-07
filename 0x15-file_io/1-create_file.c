304ede832f7f994b4552eec18a8befd8da013dbf
Molatelo-Pro <molatelomashishi@gmail.com>
2024-07-07 00:48:58 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,34 +9,32 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, write_res, length = 0;
+int fd, write_res, length = 0;
 
-    if (filename == NULL)
-    {
-        return (-1);
-    }
+if (filename == NULL)
+{
+return (-1);
+}
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+{
+return (-1);
+}
+if (text_content != NULL)
+{
+while (text_content[length])
+{
+length++;
+}
+write_res = write(fd, text_content, length);
+if (write_res == -1)
+{
+close(fd);
+return (-1);
+}
+}
+close(fd);
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-    {
-        return (-1);
-    }
+return (1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[length])
-	{
-            length++;
-        }
-
-        write_res = write(fd, text_content, length);
-        if (write_res == -1)
-	{
-            close(fd);
-            return (-1);
-        }
-    }
-
-    close(fd);
-    return (1);
 }
